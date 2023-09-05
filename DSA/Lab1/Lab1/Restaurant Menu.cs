1b87aa5674a6daa5b59d37f5a5ee88db8267bbd5
eMantonya <108507538+eMantonya@users.noreply.github.com>
2023-09-05 13:35:12 -0500

    Created lab1 with tests

---
+++ DSA/Lab1/Lab1/Restaurant Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class RestaurantMenu
+    {
+        public string GetDishRecommendation(string proteinChoice)
+        {
+            switch (proteinChoice.ToLower())
+            {
+                case "beef":
+                    return "Hamburger";
+                case "pepperoni":
+                    return "Pizza";
+                case "tofu":
+                    return "Tofu Fried Rice";
+                default:
+                    return "That protein is not available.";
+
+            }
+            
+        }
+    }
+}
---
+++ DSA/Lab1/Lab1_Test/ProteinSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+using Lab1;
+
+namespace Lab1_Test
+{
+    [TestClass]
+    public class ProteinSelection
+    {
+
+        RestaurantMenu menu;
+        [TestInitialize]
+        public void TestSetup()
+        {
+            menu = new RestaurantMenu();
+        }
+
+        [TestMethod]
+        [DataRow("Beef", "Hamburger")]
+        [DataRow("Pepperoni", "Pizza")]
+        [DataRow("Tofu", "Tofu Fried Rice")]
+        public void TestWithExpectedProtienTypeShouldReturnCorrespondingDishes(string proteinChoice, string menuItem)
+        {
+            string dish = menu.GetDishRecommendation(proteinChoice);
+            Assert.AreEqual(menuItem, dish, true);
+        }
+        [TestMethod]
+        public void TestWith_UNExpected_ProteinTypeShouldReturnMessageProteinNotAvailable()
+        {
+            string dish = menu.GetDishRecommendation("fish");
+            Assert.AreEqual("That protein is not available.", dish, true);
+        }
+    }
+}
