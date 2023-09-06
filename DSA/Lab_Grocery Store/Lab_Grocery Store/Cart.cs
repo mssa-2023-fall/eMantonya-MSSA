765471121abd0184b3ab1ea3a5ea007f0b9d5ecb
eMantonya <108507538+eMantonya@users.noreply.github.com>
2023-09-06 12:14:30 -0500

    Grocery lab complete

---
+++ DSA/Lab_Grocery Store/Lab_Grocery Store/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Grocery_Store
+{
+    internal class Cart
+    {
+        public Dictionary<string, float> Contents;
+    }
+}
